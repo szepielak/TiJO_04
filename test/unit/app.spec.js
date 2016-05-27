0a3dde202cb98896a9e692da1e236a7a3304026a
arekdy <arek.dyn@gmail.com>
2016-05-27 14:52:36 +0200

    New unit tests.

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -35,4 +35,44 @@ describe('app', function () {
             expect(app.divide(101, 0)).toEqual(false);
         });
     });
+    describe('getDescendingNumbers function', function(){
+        it('should return false when numberFrom<numberTo', function(){
+            expect(app.getDescendingNumbers(5, 6)).toEqual(false);
+        });
+        it('should return false when numberFrom!==number', function(){
+            expect(app.getDescendingNumbers('5', 9)).toEqual(false);
+        });
+        it('sholud return string when numberFrom is more than numberTo', function(){
+            expect(app.getDescendingNumbers(9, 3)).toEqual('9 8 7 6 5 4 3');
+        });
+    });
+    describe('areaOfTrapezoid function', function(){
+        it('should return false when a, b or h is less than 0', function(){
+            expect(app.areaOfTrapezoid(-2, -4, -9)).toEqual(false);
+        });
+        it('should calculate area of trapezoid when a, b, c are more than 0', function(){
+            expect(app.areaOfTrapezoid(5, 6, 9)).toEqual(49.5);
+        });
+        it('should return false when a, b or c is not a number', function(){
+            expect(app.areaOfTrapezoid('5', 7, 3)).toEqual(false);
+        });
+    });
+    describe('maxArray function', function(){
+        it('should return false when array contain not numerical', function(){
+            expect(app.maxArray([2, 3, 8, 'abc', 4])).toEqual(false);
+        });
+        it('should return max number from array', function(){
+            expect(app.maxArray([2, 3, 8, 6, 4])).toEqual(8);
+            expect(app.maxArray([10, 55, 66, 22])).toEqual(66);
+        });
+    });
+    describe('squareOdd function', function(){
+        it('should return array where elements are square from base array if elements from base array are even', function(){
+            expect(app.squareOdd([3, 5, 9])).toEqual([9, 25, 81]);
+        });
+        it('should return array where strings and odd numbers from base array are unchanged', function(){
+            expect(app.squareOdd([8, 'abc', 10, 'lkj'])).toEqual([8, 'abc', 10, 'lkj']);
+        });
+    })
 });
+
